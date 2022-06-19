06c659b6580b8735dddcad14782e142fd23c9618
Jovan Milinkovic <milinkovicjovanbusiness@gmail.com>
2022-06-19 23:35:42 +0200

    update

--- src/script.js
+++ src/script.js
@@ -1,7 +1,9 @@
+// window.addEventListener('click', outsideClick);
+
 function openNav() {
   document.getElementById('mySidenav').style.width = '70%';
   document.getElementById('main').style.marginLeft = '0%';
-  document.body.style.backgroundColor = 'rgba(0,0,0,0.4)';
+  document.body.style.backgroundColor = 'rgba(0,0,0,0.6)';
 }
 
 function closeNav() {
@@ -9,3 +11,28 @@ function closeNav() {
   document.getElementById('main').style.marginLeft = '0';
   document.body.style.backgroundColor = 'white';
 }
+
+// // Close If Outside Click
+// function outsideClick(e) {
+//   if (e.target == mySidenav) {
+//     mySidenav.style.width = '0';
+//   }
+// }
+
+const expertise = document.querySelector('.expertise');
+
+for (var i = 0; i < expertise.children.length; i++) {
+  if (i >= 8) {
+    expertise.children[i].classList.add('hidden');
+    expertise.children[i].classList.add('hidden-item');
+  }
+}
+
+const button = document.querySelector('.toggle-button');
+const hiddenItems = document.querySelectorAll('.hidden-item');
+let isHidden = true;
+button.addEventListener('click', () => {
+  button.textContent = isHidden ? 'View less' : 'View more';
+  isHidden = !isHidden;
+  hiddenItems.forEach((item) => item.classList.toggle('hidden'));
+});
