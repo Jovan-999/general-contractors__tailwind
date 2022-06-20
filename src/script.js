3d1b64a5fb8aa216e88f695dbc7f441c4d46d85b
Jovan Milinkovic <milinkovicjovanbusiness@gmail.com>
2022-06-20 11:57:04 +0200

    update

--- src/script.js
+++ src/script.js
@@ -2,13 +2,11 @@
 
 function openNav() {
   document.getElementById('mySidenav').style.width = '70%';
-  document.getElementById('main').style.marginLeft = '0%';
   document.body.style.backgroundColor = 'rgba(0,0,0,0.6)';
 }
 
 function closeNav() {
   document.getElementById('mySidenav').style.width = '0';
-  document.getElementById('main').style.marginLeft = '0';
   document.body.style.backgroundColor = 'white';
 }
 
